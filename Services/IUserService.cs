d43fef71be12333d4203d283972d7ddf6c89ba50
Maksimiliyan Taraniuk <s32797@pjwstk.edu.pl>
2026-03-23 20:50:03 +0100

    feat(service): add user service

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,6 +1,11 @@
+using APBD_Cw2_s32797.Models;
+
 namespace APBD_Cw2_s32797.Services;
 
 public interface IUserService
 {
+    void AddUser(User user);
+    List<User> GetAllUsers();
     
+    User GetUserById(int userId);
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,38 @@
+using APBD_Cw2_s32797.Exceptions;
+using APBD_Cw2_s32797.Models;
+
 namespace APBD_Cw2_s32797.Services;
 
-public class UserService : IRentalService
+public class UserService : IUserService
 {
+    private readonly List<User> _users = new();
+
+    public void AddUser(User user)
+    {
+        if (user == null)
+        {
+            throw new ArgumentNullException(nameof(user));
+        }
+        
+        _users.Add(user);
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return _users;
+    }
+
+    public User GetUserById(int userId)
+    {
+        var user = _users.FirstOrDefault(x => x.Id == userId);
+
+        if (user == null)
+        {
+            throw new UserNotFoundException(userId);
+        }
+        
+        return user;
+    }
+    
     
 }
