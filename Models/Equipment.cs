cd307f85b84d08dee5da43650e3d196bae1fa3eb
Maksimiliyan Taraniuk <s32797@pjwstk.edu.pl>
2026-03-23 19:02:38 +0100

    feat(domain) add base user and equipment models

--- Models/Equipment.cs
+++ Models/Equipment.cs
@@ -1,6 +1,34 @@
 namespace APBD_Cw2_s32797.Models;
 
+using APBD_Cw2_s32797.Enums;
+
 public abstract class Equipment
 {
+    private static int _nextId = 1;
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public EqiupmentStatus EqiupmentStatus { get; private set; }
+
+    protected Equipment(string name)
+    {
+        Id = _nextId++;
+        Name = name;
+        EqiupmentStatus = EqiupmentStatus.Available;
+    }
+
+    public void setAvailable()
+    {
+        EqiupmentStatus = EqiupmentStatus.Available;
+    }
+
+    public void setUnavailable()
+    {
+        EqiupmentStatus = EqiupmentStatus.Unavailable;
+    }
+
+    public void setBorrowed()
+    {
+        EqiupmentStatus = EqiupmentStatus.Borrowed;
+    }
     
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,26 @@
+using APBD_Cw2_s32797.Enums;
+
 namespace APBD_Cw2_s32797.Models;
 
 public  abstract class User
 {
+    private static int _nextId = 1;
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public UserType UserType { get; }
+
+    protected User(string firstName, string lastName, UserType userType)
+    {
+        Id = _nextId++;
+        FirstName = firstName;
+        LastName = lastName;
+        UserType = userType;
+    }
     
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
 }
