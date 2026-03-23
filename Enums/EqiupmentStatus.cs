b2b78f41cd3dbcb7ec9e671bca9446aec033327b
Maksimiliyan Taraniuk <s32797@pjwstk.edu.pl>
2026-03-23 19:01:34 +0100

    feat(domain): add equipment and user enums

--- Enums/EqiupmentStatus.cs
+++ Enums/EqiupmentStatus.cs
@@ -2,5 +2,7 @@ namespace APBD_Cw2_s32797.Enums;
 
 public enum EqiupmentStatus
 {
-    
+    Available,
+    Unavailable,
+    Borrowed
 }
--- Enums/UserType.cs
+++ Enums/UserType.cs
@@ -2,5 +2,6 @@ namespace APBD_Cw2_s32797.Enums;
 
 public enum UserType
 {
-    
+    Student,
+    Employee
 }
